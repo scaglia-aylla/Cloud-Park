ccba8928db3b735e2bd5ea2261bfd8596080487c
Aylla Scaglia <aylla@scaglia.com.br>
2022-11-19 19:32:32 -0300

    "Definindo a versão do Java"

---
+++ src/main/java/digital/Cloud/Park/Controller/ParkingController.java
@@ -0,0 +1,30 @@
+package digital.Cloud.Park.Controller;
+
+import digital.Cloud.Park.Controller.dto.ParkingDTO;
+import digital.Cloud.Park.Controller.mapper.ParkingMapper;
+import digital.Cloud.Park.Model.Parking;
+import digital.Cloud.Park.Service.ParkingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+    @GetMapping
+    public List<ParkingDTO> findAll(){
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
+    }
+}
---
+++ src/main/java/digital/Cloud/Park/Controller/mapper/ParkingMapper.java
@@ -0,0 +1,22 @@
+package digital.Cloud.Park.Controller.mapper;
+
+import digital.Cloud.Park.Controller.dto.ParkingDTO;
+import digital.Cloud.Park.Model.Parking;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO parkingDTO(Parking parking){
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
+        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+    }
+}
