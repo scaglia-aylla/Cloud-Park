8656dde7d6b13d7cd7930b7c69c5f5ba5e410931
Aylla Scaglia <aylla@scaglia.com.br>
2022-11-19 19:32:47 -0300

    "Definindo a versão do Java"

---
+++ src/main/java/digital/Cloud/Park/Service/ParkingService.java
@@ -0,0 +1,30 @@
+package digital.Cloud.Park.Service;
+
+import digital.Cloud.Park.Model.Parking;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class ParkingService {
+
+    private static Map<String, Parking> parkingMap = new HashMap();
+
+    static {
+        var id = getUUID();
+        Parking parking = new Parking(id, "DMS-1212", "SC", "GOL", "VERMELHO");
+        parkingMap.put(id, parking);
+    }
+
+    public List<Parking> findAll(){
+        return parkingMap.values().stream().collect(Collectors.toList());
+    }
+
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
